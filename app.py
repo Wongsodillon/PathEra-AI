9f16932c21e108d9f5c4bc75687afc27195ae96c
albrtleonardi <leoneeroooo@gmail.com>
2024-09-26 11:54:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
+import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
 import pandas as pd
+import nltk
 from JobMatcher.job_matcher import JobRecommender
 from InterviewSimulation.evaluator import Evaluator
 
+# Download NLTK stopwords if not already present
+nltk.download('stopwords')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://pathera.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:5005/"}})
 
 db_config = {
     'host': os.getenv('DB_HOST', 'localhost'),  
@@ -17,7 +25,6 @@ db_config = {
 }
 
 model = JobRecommender()
-# evaluator = Evaluator(db_config=db_config, model_name="./InterviewSimulation/model/multiple_negatives_11")
 evaluator = Evaluator(db_config=db_config)
 
 @app.route('/recommend', methods=['POST'])
@@ -25,8 +32,11 @@ def recommend():
     if request.is_json:
         try:
             user_data = request.get_json()
+            if 'skills' not in user_data:
+                return jsonify({"error": "Missing 'skills' field in the request body"}), 400
+            
             user_data_df = pd.DataFrame([user_data])
-            print(user_data_df['skills'])
+            logging.info(f"User skills: {user_data_df['skills'].values}")
             result, skill_matches = model.recommend_jobs(user_data_df)
             
             return jsonify({
@@ -35,11 +45,13 @@ def recommend():
                 'skill_matches': skill_matches.to_dict(orient='records')
             }), 200
         except FileNotFoundError as e:
+            logging.exception("File not found")
             return jsonify({'success': False, 'error': f"File not found: {str(e)}"}), 500
         except pd.errors.EmptyDataError:
+            logging.exception("Empty CSV error")
             return jsonify({'success': False, 'error': "The CSV file is empty."}), 500
         except Exception as e:
-            print("Exception occurred:", e)
+            logging.exception("Exception occurred")
             return jsonify({'success': False, 'error': str(e)}), 500
     else:
         return jsonify({"error": "Request must be JSON"}), 400
@@ -59,8 +71,13 @@ def evaluate():
         score = evaluator.evaluate(question_id, user_answer)
         return jsonify({"success": True, "score": score }), 200
     except Exception as e:
+        logging.exception("Error during evaluation")
         return jsonify({"error": str(e)}), 500
-    return jsonify({"success": True}), 200
+
+# Optional: Health Check Endpoint
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "Healthy"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, port=int(os.getenv('PORT', 5020)))
